957f1a6c54d3998ee88d664cc507ed4bea560246
Shaahin Shahbazi <shaahin@gmail.com>
2015-11-25 11:18:59 -0600

    Fragment interacts with Activity via interface implementation

--- app/src/main/java/com/example/schann/fragactivityinteract/MainActivity.java
+++ app/src/main/java/com/example/schann/fragactivityinteract/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -30,7 +31,7 @@ public class MainActivity extends AppCompatActivity implements MainActivityFragm
 
     @Override
     public void onArticleSelected(int length) {
-
+      Log.d("LENGTH", "String length is now " + length );
     }
 
     @Override
--- app/src/main/java/com/example/schann/fragactivityinteract/MainActivityFragment.java
+++ app/src/main/java/com/example/schann/fragactivityinteract/MainActivityFragment.java
@@ -34,18 +34,33 @@ public class MainActivityFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_main, container, false);
+
         txt = (EditText) v.findViewById(R.id.editText);
+        txt.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                mCallback.onArticleSelected(s.length());
+                // Log.d("LENGTH", s.length()+" ");
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
 
         return v;
     }
 
 
-
     @Override
     public void onAttach(Activity activity) {
         super.onAttach(activity);
-
-
 
         // This makes sure that the container activity has implemented
         // the callback interface. If not, it throws an exception
